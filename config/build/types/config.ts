88e67148b0f4a9b8f9fddfd209590e4cf682ce56
Woood99 <85289176+Woood99@users.noreply.github.com>
2025-10-30 00:58:09 +0300

    add config

---
+++ config/build/types/config.ts
@@ -0,0 +1,21 @@
+import { EntryObject } from 'webpack';
+
+export type BuildMode = 'development' | 'production';
+
+export interface BuildPaths {
+   entry: EntryObject;
+   build: string;
+   html: string;
+}
+
+export interface BuildOptions {
+   mode: BuildMode;
+   paths: BuildPaths;
+   isDev: boolean;
+   port: number;
+}
+
+export interface BuildEnv {
+   mode: BuildMode;
+   port: number;
+}
